1bf6a7e02ea89ca7ac1150be705d7f362e8b9c26
Sergei <mindalev@ya.ru>
2024-05-14 12:28:06 +0300

    django_init

---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def catalog_index(request):
+
+    return render(request, 'index.html')
+
+
+def catalog_contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"Сообщение от {name}, телефон: {phone} - {message}")
+    return render(request, 'contacts.html')
